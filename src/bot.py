a9c9902cbf983119397848ce2fd262609c3c05d6
Kalev <kallikalev@gmail.com>
2024-11-14 12:18:52 -0800

    Reworked grouch include, and set up basic information grabing

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,9 @@
 import discord
 import asyncio
 
+from courses import Course
+from datetime import datetime
+
 from token_fetcher import get_token
 token = get_token()
 
@@ -18,5 +21,20 @@ async def on_message(message: discord.Message):
     message_author = message.author
     message_channel = message.channel
     print(f'New message -> {message_author} said: {message_content}')
+    if not message_author.bot:
+        await message_channel.send(content="Fetching info for CRN: " + message_content)
+        await message_channel.send(content=fetch_course(message_content))
+
+def fetch_course(crn: str) -> str:
+    season = "spring"
+    now = datetime.now()
+    term = ''
+
+    if season.lower() == 'spring':
+        term = f'{now.year + 1}' + '02' if now.month > 4 else f'{now.year}' + '02'
+    else:
+        term = f'{now.year}' + '05' if season.lower() == 'summer' else f'{now.year}' + '08'
+    course = Course(crn, term)
+    return str(course)
 
 client.run(token)
