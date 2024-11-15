12061e75cb38fbe9b0f1d0f09219df7a715a9569
Kalev <kallikalev@gmail.com>
2024-11-15 11:10:15 -0800

    Make command arguments optional and handle missing argument errors

--- src/bot.py
+++ src/bot.py
@@ -19,6 +19,10 @@ bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
 
 @bot.command()
 async def info(ctx: commands.Context, *crns):
+    print(f"\"{ctx.message.content}\" from user {ctx.author.name}")
+    if len(crns) == 0:
+        await ctx.reply(f"Please specify at least one CRN")
+        return        
     courses: list[Course] = [Course(crn, "202502") for crn in crns]
     for course in courses:
         if course.data['seats'] == -1:
@@ -28,28 +32,33 @@ async def info(ctx: commands.Context, *crns):
     await ctx.reply('\n'.join(course_info))
 
 @bot.command()
-async def track(ctx: commands.Context, crn: str):
+async def track(ctx: commands.Context, crn: str = None):
+    print(f"\"{ctx.message.content}\" from user {ctx.author.name}")
     if ctx.guild is None:
-        await ctx.reply("Error: Can only call $track from a channel in a server")
-    else:
-        for request in global_request_list:
-            if request.crn == crn and request.userId == ctx.author.id:
-                if request.channelId == ctx.channel.id:
-                    await ctx.reply(f"You are already tracking CRN: `{crn}`")
-                else:
-                    await ctx.reply(f"You are already tracking CRN: `{crn}` in channel <#{request.channelId}>")
-                return
-        newRequest = TrackRequest(crn,"202502",ctx.author.id,ctx.channel.id)
-        if newRequest.course.data['seats'] == -1:
-            await ctx.reply(f"Error retrieving CRN: `{crn}`")
+        await ctx.reply("Can only call $track from a channel in a server")
+        return
+    if crn is None:
+        await ctx.reply("Please specify a CRN")
+        return
+    for request in global_request_list:
+        if request.crn == crn and request.userId == ctx.author.id:
+            if request.channelId == ctx.channel.id:
+                await ctx.reply(f"You are already tracking CRN: `{crn}`")
+            else:
+                await ctx.reply(f"You are already tracking CRN: `{crn}` in channel <#{request.channelId}>")
             return
-        else:
-            global_request_list.append(newRequest)
-            save_request_list(global_request_list)
-            await ctx.reply(f"Now tracking CRN: `{crn}`")
+    newRequest = TrackRequest(crn,"202502",ctx.author.id,ctx.channel.id)
+    if newRequest.course.data['seats'] == -1:
+        await ctx.reply(f"Error retrieving CRN: `{crn}`")
+        return
+    else:
+        global_request_list.append(newRequest)
+        save_request_list(global_request_list)
+        await ctx.reply(f"Now tracking CRN: `{crn}`")
 
 @bot.command()
 async def tracking(ctx: commands.Context):
+    print(f"\"{ctx.message.content}\" from user {ctx.author.name}")
     linked_requests: list[TrackRequest] = []
     for request in global_request_list:
         if request.userId == ctx.author.id:
@@ -63,7 +72,11 @@ async def tracking(ctx: commands.Context):
         await ctx.reply(message)
 
 @bot.command()
-async def untrack(ctx: commands.Context, crn: str):
+async def untrack(ctx: commands.Context, crn: str = None):
+    print(f"\"{ctx.message.content}\" from user {ctx.author.name}")
+    if crn is None:
+        await ctx.reply("Please specify a CRN")
+        return
     for request in global_request_list:
         if request.userId == ctx.author.id and request.crn == crn:
             global_request_list.remove(request)
@@ -75,6 +88,7 @@ async def untrack(ctx: commands.Context, crn: str):
 bot.remove_command('help')
 @bot.command()
 async def help(ctx: commands.Context):
+    print(f"\"{ctx.message.content}\" from user {ctx.author.name}")
     await ctx.reply(
     """
     Commands:
@@ -93,6 +107,7 @@ async def check_crn():
             text = (f"<@{request.userId}> course status changed:" 
             + "\n" + str(request.course))
             messages.append((request.channelId,text))
+        print(f"Scraped {request.course.name}")
     for message in messages:
         await bot.get_channel(message[0]).send(message[1],
                                                allowed_mentions=discord.AllowedMentions(users=True))
