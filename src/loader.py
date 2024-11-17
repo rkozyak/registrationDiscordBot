c9af3a21952222399abdfc2dc4ab5be911d299f1
Kalev <kallikalev@gmail.com>
2024-11-16 19:33:31 -0800

    Parallelize scraping when loading requests on startup

--- src/loader.py
+++ src/loader.py
@@ -1,14 +1,20 @@
 import json
 from tracking import TrackRequest
 
+import threading
 
 def load_request_list() -> list[TrackRequest]:
     try:
         with open("/var/data/saved_requests.json", 'r') as file:
             data = json.load(file)
             trackList = []
-            for item in data:
+            def load_request(item):
                 trackList.append(TrackRequest(item['crn'],item['term'],item['userId'],item['channelId']))
+            threads = [threading.Thread(target=load_request,args=(item,)) for item in data]
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
             return trackList
     except:
         return []
